32cc71d86d20a4828d9d75c1f8d3d273778e177e
makkaro <makowskikarol@outlook.com>
2022-09-03 12:26:23 +0200

    basic deps

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-void async function () {}()
+require('dotenv').config()
+
+var express = require('express')
+
+var server = express()
+
+void async function () {
+    server.listen(process.env.PORT, function () {
+        console.info(`Server is listening on port ${process.env.PORT}...`)
+    })
+}()
