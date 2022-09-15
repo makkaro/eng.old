20658b57bd367cd008b566d4c801c65a10d1bc60
makkaro <makowskikarol@outlook.com>
2022-09-15 15:32:39 +0200

    seed

--- auxp/seed.js
+++ auxp/seed.js
@@ -1,10 +1,62 @@
 require('dotenv').config()
 
+var chance = new (require('chance'))
+
 var db = require('../db')
 
 /* -------------------------------------------------------------------------- */
 
+chance.mixin({
+    category: () => ({
+        hidden: chance.bool({likelihood: 0}),
+        name: chance.word()
+    }),
+    manufacturer: () => ({
+        hidden: chance.bool({likelihood: 0}),
+        name: chance.company()
+    }),
+    product: () => ({
+        hidden: chance.bool(),
+        name: chance.word(),
+        cost: chance.floating({min: 0, max: 9999.99, fixed: 2})
+    })
+})
+
 async function seed() {
+    Array.prototype.random = function () {
+        return this[Math.floor(Math.random() * this.length)]
+    }
+
+    function generator(model, amt) {
+        return async function* () {
+            for (var i = 0; i < amt; ++i) {
+                yield await db[model].create(chance[model]())
+            }
+        }
+    }
+
+    async function entities(model, amt) {
+        var entities = Array()
+
+        for await (var value of await generator(model, amt)()) {
+            entities.unshift(value)
+        }
+
+        return entities
+    }
+
+    var categories = await entities('category', 6),
+        manufacturers = await entities('manufacturer', 8),
+        products = await entities('product', 30)
+
+
+    products.forEach(product => { with (product) {
+        setCategory(categories.random())
+        setManufacturer(manufacturers.random())
+    }})
+
+    delete Array.prototype.random
+
     await db.user.create({username: 'genghis', password: 'genghis1'})
 }
 
