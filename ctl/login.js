3e96b620fbebf8f670c7dfc7d06ca8e5a7444ea4
makkaro <makowskikarol@outlook.com>
2022-09-20 18:41:19 +0200

    persistent cart proto

--- ctl/login.js
+++ ctl/login.js
@@ -15,12 +15,32 @@ module.exports.get = async function (req, res) {
 
 module.exports.post = async function (req, res) {
     var user = await db.user.scope('full').findOne({
-        where: {username: req.body.username}
+        where: {username: req.body.username},
+        include: db.cart
     })
 
     if (!user || await user.unauthenticated(req.body.password)) {
         throw errors.unauthorized('invalid credentials')
     }
+
+    if (!user.cart) {
+        user.cart = await user.createCart()
+
+
+        if (req.body.cart) {
+            var local = JSON.parse(req.body.cart)
+        }
+
+        if (local){
+            for (var item of local.items) {
+                var product = await db.product.findByPk(item.id)
+                user.cart.addProduct(product, {through: {amount: item.amount}})
+            }
+        }
+
+    }
+
+    console.log(await user.cart.getProducts())
 
     req.session.authenticated = true
     req.session.user = user
