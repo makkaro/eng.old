28a8839ec6f5d0b8d3150be73c2246350129f939
makkaro <makowskikarol@outlook.com>
2022-10-12 21:57:53 +0200

    admin - home

--- auxp/admin/index.js
+++ auxp/admin/index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 require('express-async-errors')
 
 var express = require('express'),
-    methodOverride = require('method-override')
+    methodOverride = require('method-override'),
+    path = require('path')
 
 var db = require('../../db'),
     routes = require('./routes')
@@ -15,6 +16,7 @@ server.set('views', './auxp/admin/views')
 server.set('view engine', 'ejs')
 
 server.use(methodOverride('_method'))
+server.use(express.static(path.join(__dirname, 'public')))
 server.use(express.urlencoded({extended: true}))
 server.use(express.json())
 server.use('/', routes)
